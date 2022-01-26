04b38722ed03dd4b057360515d21d07c09fe698e
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2022-01-26 12:41:55 +0300

    add LandAndWavesApp

---
+++ 06_LandAndWaves/build_windows.bat
@@ -0,0 +1,3 @@
+cl App6b.cpp LandAndWavesApp.cpp FrameResource.cpp Waves.cpp ..\Common\d3dApp.cpp ..\Common\d3dUtils.cpp ..\Common\GameTimer.cpp ..\Common\MathHelper.cpp ^
+..\Common\GeometryGenerator.cpp ^
+/DSUBSYSTEM_WINDOWS  /EHsc /I..\Common -DUNICODE /link gdi32.lib user32.lib /SUBSYSTEM:WINDOWS
