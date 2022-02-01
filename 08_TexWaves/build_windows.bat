a3933ccda42c5e91cbfb4cbc5c52b27686d7850b
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2022-02-01 13:01:20 +0300

    add tex waves app

---
+++ 08_TexWaves/build_windows.bat
@@ -0,0 +1,3 @@
+cl App8b.cpp TexWavesApp.cpp FrameResource.cpp Waves.cpp ..\Common\d3dApp.cpp ..\Common\d3dUtils.cpp ..\Common\GameTimer.cpp ..\Common\MathHelper.cpp ^
+..\Common\GeometryGenerator.cpp ..\Common\DDSTextureLoader.cpp ^
+/DSUBSYSTEM_WINDOWS  /EHsc /I..\Common -DUNICODE /link gdi32.lib user32.lib /SUBSYSTEM:WINDOWS
