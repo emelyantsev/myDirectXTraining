13744467a5b39be4b1bf7fe7bf3ddc35ea17e6b5
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2022-02-04 17:44:46 +0300

    Add stencil demo app

---
+++ 10_StencilDemo/build_windows.bat
@@ -0,0 +1,3 @@
+cl App10.cpp StencilApp.cpp FrameResource.cpp ..\Common\d3dApp.cpp ..\Common\d3dUtils.cpp ..\Common\GameTimer.cpp ..\Common\MathHelper.cpp ^
+..\Common\GeometryGenerator.cpp ..\Common\DDSTextureLoader.cpp ^
+/DSUBSYSTEM_WINDOWS  /EHsc /I..\Common -DUNICODE /link gdi32.lib user32.lib /SUBSYSTEM:WINDOWS
