6090e96e59a6fb1e91c8a9a20aad32b745cc1b4d
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2022-02-07 15:53:16 +0300

    add TreeBillboards app

---
+++ 11_TreeBillboards/App11.cpp
@@ -0,0 +1,34 @@
+#include "TreeBillboardsApp.h"
+
+
+#if defined(SUBSYSTEM_WINDOWS)
+	int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+				   PSTR cmdLine, int showCmd) {
+#else
+    int main() {
+#endif
+
+    TreeBillboardsApp app;
+
+    try {
+        
+        if ( !app.Initialize() ) {
+
+            std::cerr << "Failed to initialize" << std::endl;
+            return EXIT_FAILURE;
+        } 
+        else {
+
+            std::cerr << "Initialized OK" << std::endl;
+        }
+
+        return app.Run() ;
+    }
+    catch ( DxException& e )
+    {
+        
+        ::MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+        return EXIT_FAILURE;
+    }
+
+}
---
+++ 11_TreeBillboards/build_console.bat
@@ -0,0 +1,3 @@
+cl App11.cpp TreeBillboardsApp.cpp FrameResource.cpp Waves.cpp ..\Common\d3dApp.cpp ..\Common\d3dUtils.cpp ..\Common\GameTimer.cpp ..\Common\MathHelper.cpp ^
+..\Common\GeometryGenerator.cpp ..\Common\DDSTextureLoader.cpp ^
+/EHsc /I ..\Common -DUNICODE /link gdi32.lib user32.lib
