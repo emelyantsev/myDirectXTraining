77b8e27129dc216b9b8aa8ef5d19ae0d38b1326d
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2022-02-11 18:27:58 +0300

    add blur example compute shader

---
+++ 12_Blur/build_console.bat
@@ -0,0 +1,3 @@
+cl App12a.cpp BlurApp.cpp BlurFilter.cpp FrameResource.cpp Waves.cpp ..\Common\d3dApp.cpp ..\Common\d3dUtils.cpp ..\Common\GameTimer.cpp ..\Common\MathHelper.cpp ^
+..\Common\GeometryGenerator.cpp ..\Common\DDSTextureLoader.cpp ^
+/EHsc /I ..\Common -DUNICODE /Zi /link gdi32.lib user32.lib
