d123b94de1f47d439d08b58d6fc4a35b193e10d3
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2022-02-14 18:07:50 +0300

    Add Sobel Filter application

---
+++ 12_SobelFilter/build_console.bat
@@ -0,0 +1,4 @@
+cl App12d.cpp SobelApp.cpp FrameResource.cpp GPUWaves.cpp SobelFilter.cpp RenderTarget.cpp ^
+..\Common\d3dApp.cpp ..\Common\d3dUtils.cpp ..\Common\GameTimer.cpp ..\Common\MathHelper.cpp ^
+..\Common\GeometryGenerator.cpp ..\Common\DDSTextureLoader.cpp ^
+/EHsc /I ..\Common -DUNICODE /Zi /link gdi32.lib user32.lib
---
+++ 12_SobelFilter/build_windows.bat
@@ -0,0 +1,4 @@
+cl App12d.cpp SobelApp.cpp FrameResource.cpp GPUWaves.cpp SobelFilter.cpp RenderTarget.cpp ^
+..\Common\d3dApp.cpp ..\Common\d3dUtils.cpp ..\Common\GameTimer.cpp ..\Common\MathHelper.cpp ^
+..\Common\GeometryGenerator.cpp ..\Common\DDSTextureLoader.cpp ^
+/O2 /Oi /DSUBSYSTEM_WINDOWS  /EHsc /I..\Common -DUNICODE /link gdi32.lib user32.lib /SUBSYSTEM:WINDOWS
