9bca3fe77361be9f53f590cfe2155c59f32d0b4d
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2022-02-14 16:55:27 +0300

    add GPU Waves compute shader application

---
+++ 12_WavesCS/App12c.cpp
@@ -0,0 +1,35 @@
+#include "WavesCSApp.h"
+
+// Use W and S to increase/decrease bluring sigma
+
+#if defined(SUBSYSTEM_WINDOWS)
+	int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+				   PSTR cmdLine, int showCmd) {
+#else
+    int main() {
+#endif
+
+    WavesCSApp app;
+
+    try {
+        
+        if ( !app.Initialize() ) {
+
+            std::cerr << "Failed to initialize" << std::endl;
+            return EXIT_FAILURE;
+        } 
+        else {
+
+            std::cerr << "Initialized OK" << std::endl;
+        }
+
+        return app.Run() ;
+    }
+    catch ( DxException& e )
+    {
+        
+        ::MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+        return EXIT_FAILURE;
+    }
+
+}
