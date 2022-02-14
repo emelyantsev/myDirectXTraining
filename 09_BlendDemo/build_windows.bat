bbc274eb3ea002ff8d73a1b64628cbb3ac009c9c
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2022-02-14 16:01:57 +0300

    add optimization options

--- 09_BlendDemo/build_windows.bat
+++ 09_BlendDemo/build_windows.bat
@@ -1,3 +1,3 @@
 cl App9.cpp BlendApp.cpp FrameResource.cpp Waves.cpp ..\Common\d3dApp.cpp ..\Common\d3dUtils.cpp ..\Common\GameTimer.cpp ..\Common\MathHelper.cpp ^
 ..\Common\GeometryGenerator.cpp ..\Common\DDSTextureLoader.cpp ^
-/DSUBSYSTEM_WINDOWS  /EHsc /I..\Common -DUNICODE /link gdi32.lib user32.lib /SUBSYSTEM:WINDOWS
+/O2 /Oi /DSUBSYSTEM_WINDOWS  /EHsc /I..\Common -DUNICODE /link gdi32.lib user32.lib /SUBSYSTEM:WINDOWS
