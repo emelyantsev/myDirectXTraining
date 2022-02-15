faff0d7b190157a929532f313ba3a5b1e856e780
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2022-02-15 16:47:37 +0300

    Add Bezier tesselation application

---
+++ 13_BezierPatch/build_console.bat
@@ -0,0 +1,3 @@
+cl App13b.cpp BezierPatchApp.cpp FrameResource.cpp ..\Common\d3dApp.cpp ..\Common\d3dUtils.cpp ..\Common\GameTimer.cpp ..\Common\MathHelper.cpp ^
+..\Common\GeometryGenerator.cpp ..\Common\DDSTextureLoader.cpp ^
+/EHsc /I ..\Common -DUNICODE /Zi /link gdi32.lib user32.lib
