0c724ad8b91c73f100cb1524c13b24429c09cd91
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2022-02-15 15:31:35 +0300

    add basic tesselation app

---
+++ 13_BasicTesselation/build_console.bat
@@ -0,0 +1,3 @@
+cl App13a.cpp BasicTessellationApp.cpp FrameResource.cpp ..\Common\d3dApp.cpp ..\Common\d3dUtils.cpp ..\Common\GameTimer.cpp ..\Common\MathHelper.cpp ^
+..\Common\GeometryGenerator.cpp ..\Common\DDSTextureLoader.cpp ^
+/EHsc /I ..\Common -DUNICODE /Zi /link gdi32.lib user32.lib
